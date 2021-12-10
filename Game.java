a8044b53ada060cb62b67fa0e76ba4d0b0cd0365
YabaYoung <jiayangwu1820@gmail.com>
2021-12-09 20:24:42 -0800

    Final! Maybe

--- Game.java
+++ Game.java
@@ -2,10 +2,11 @@
 // Date: 11/30/2021
 // File: Game.java
 
+// Import Random class 
 import java.util.Random;
 
 public class Game {
-
+   // Instaiate private fields
    private String computer;
    private int userWinCount;
    private int computerWinCount;
@@ -13,35 +14,52 @@ public class Game {
    
    // Constructor
    public Game() {
-   
+      // Set the default values for the Game class when created
       computer = "";
-      
       userWinCount = 0;
-      
       computerWinCount = 0;
    }
-      
+   
+   // Getter that returns user's win count
+   public int getPlayerWinCount() {
+      return userWinCount;
+   }
+   
+   // Getter that returns computer's win count
+   public int getComputerWinCount() {
+      return computerWinCount;
+   }  
+   
+   // Getter that returns the computer move    
    public String getComputerMove() {
-      
+      // This decides what move the computer will use. Basically the random class will randomly generate a number from 0-2 because the array length will be 3,
+      // whatever number that it lands on that's will be the computer's move. So if it is nextInt(0) the computer move will be rock, if nextInt(1) then the computer move will be paper
       computer = paperRockScissors[new Random().nextInt(paperRockScissors.length)]; 
-      
       return computer;
    } 
-   public void printWinOrLoss() {
    
-      if (getComputerWinCount() > getPlayerWinCount()) {
-         System.out.println("Dang! You got beaten by a computer. That means you are worse than a bot.\n"); 
-      }    
-      else if (getComputerWinCount() < getPlayerWinCount()) {
+   // Method printWinOrLoss compares the value of player win count to the computer win count
+   public void printWinOrLoss() {
+      // If computer beats the user in win count overall, then it will print a message saying the computer defeated the user
+      if (computerWinCount > userWinCount) {
+         System.out.println("Dang! You got beaten by a computer. That means you are worse than a bot xD.\n"); 
+      } 
+      // Else if user beats computer in win count overall, then it will print a message saying the user beat the computer
+      else if (computerWinCount < userWinCount) {
          System.out.println("Congratulations you won against a computer, as expected from a genius like you.\n");
       }
+      // Else if the win count is tied, it will print out a message saying it is tied
       else {
-         System.out.println("Tied?! I guess you are the same as a bot.\n");
+         System.out.println("Tied?! I guess you are the same as a bot LOL.\n");
       }
    }
    
+   // Method that takes 2 arguments, the user's move and computer's move
+   // then checks who won that round using if statements and switch statement.
+   // If computer won, increase computerWinCount by 1, vice versa for userWinCount
    public void whoWon(String user, String computerInput) {
-      
+   
+      // This statement will run if the user inputs "rock"
       if (user.toLowerCase().equals("rock")) {
          switch (computer) {
             case "paper":
@@ -60,6 +78,7 @@ public class Game {
                break;              
          }
       }
+      // This statement will run if the user inputs "paper"
       else if (user.toLowerCase().equals("paper")) {  
          switch (computer) {
             case "scissors":
@@ -77,7 +96,8 @@ public class Game {
                System.out.println("That was a tie!");
                break;
          } 
-      }              
+      } 
+      // This statement will run if the user inputs "scissors"             
       else if (user.toLowerCase().equals("scissors")) {  
          switch (computer) {
             case "rock":
@@ -96,18 +116,30 @@ public class Game {
                break; 
          }
       }
+      // If the user inputs something that does not equal the above, then this message will print
       else {
          System.out.println("The input was invalid. Please enter rock, paper, or scissors.");
       }                            
    }
    
-   public int getPlayerWinCount() {
-      return userWinCount;
+   // Method that returns a string and takes in a integer
+   // This prints out the message once the user quits the game and final results are printed
+   public String endingMessage(int roundNum) {
+      // If the round played is less than or equal to 5, then the first if statement will run
+      if (roundNum <= 5) {
+         return "The game just started, and you are done already?! What a bummer :(";
+      }
+      // Else if round played is greater than 5 AND less than or equal to 15, this else if statment will run
+      else if (roundNum > 5 && roundNum <= 15) {
+         return "Darn it, we just got warmed up. Well I guess it is time to say goodbye.";
+      }
+      // Else if the round played is higher than 15, this will be the return value 
+      else {
+         return "Man, that was intense. You put up a good fight! Have a nice day :)";
+      }
    }
-   public int getComputerWinCount() {
-      return computerWinCount;
-   }
-}   
+}  
+ 
                
       
    
--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -2,50 +2,64 @@
 // Date: 11/30/2021
 // File: RockPaperScissor.java
 
-import java.util.Scanner;
-import java.util.Random;
+import java.util.Scanner; // Import Scanner class
+import java.util.Random; // Import Random class
 import java.io.*; // Import Java I/O classes
 
 public class RockPaperScissor { 
-   
+   // Main method
    public static void main (String[] args) throws IOException {
       
-      String rpsInput = "";
-      
+      // Create new Game called gameProcess
       Game gameProcess = new Game();
       
-      printGreetingFromFile();
-      
-      while (true) {
-      
+      // Create Scanner to get rock paper scissors input
       Scanner keyboard = new Scanner(System.in); 
       
-      System.out.println("\nPlease input what you want to use against the computer, paper, rock, or scissors? FYI, to quit playing please enter q, or quit.");
+      String rpsInput = ""; // Declare new String variable called rpsInput to store input
+      int roundNumber = 0; // Declare variable roundNumber, of type int, to keep track of what round we are on
       
-      rpsInput = keyboard.nextLine();
-         
-         if (rpsInput.toLowerCase().equals("q") || rpsInput.toLowerCase().equals("quit") || rpsInput.equals("Quit")) {
-            break;
-         } 
-           
-         else {
-            gameProcess.whoWon(rpsInput, gameProcess.getComputerMove());
-  
-            System.out.println("Computer's win count: " + gameProcess.getComputerWinCount());
-            System.out.println("User's win count: " + gameProcess.getPlayerWinCount()); 
-         }
-      }   
-      gameProcess.printWinOrLoss();            
-   }
-   public static void printGreetingFromFile() throws IOException {
+      // Create file object 
       File greetingsFile = new File("Greetings.txt");
       
+      // Create scanner that will read the created file, which is "Greetings.txt"
       Scanner fileReader = new Scanner(greetingsFile);
-     
+      
+      // Loop that will print the lines in the .txt file as long it has a next line
       while (fileReader.hasNext()) {
          System.out.println(fileReader.nextLine());   
       }
       
+      // Closes Scanner fileReader
       fileReader.close();   
-   }     
+      
+      // Infinite loop that ends when user quits
+      while (true) {
+      
+         // Increase the round number by 1 for every round played. This number will stop increasing once the user quits the game
+         roundNumber++;
+         
+         // Ask the user to input paper, rock, or scissors and store it in the String variable rpsInput
+         System.out.println("\nPlease input what you want to use against the computer, paper, rock, or scissors? FYI, to quit playing please enter q, or quit.");
+         rpsInput = keyboard.nextLine();
+         
+         // If statement that will end the game if the user inputs q or quit.
+         // Else, it will compare the user's move with the computer's move. If the computer's move beats the user's input than computer win count increments by 1, vice versa. 
+         // Also if they both use the same move, it will return as tie and neither computer or user win count increase.
+         if (rpsInput.toLowerCase().equals("q") || rpsInput.toLowerCase().equals("quit") || rpsInput.equals("Quit")) {
+            break;
+         }  
+         else {
+            gameProcess.whoWon(rpsInput, gameProcess.getComputerMove());
+            System.out.println("Computer's win count: " + gameProcess.getComputerWinCount());
+            System.out.println("User's win count: " + gameProcess.getPlayerWinCount()); 
+         }
+      }
+      // Print total rounds played to the console
+      System.out.println("Total rounds played: " + roundNumber); 
+      // Print the ending message to the console, the argument roundNumber will play a role in what message we get.
+      System.out.println(gameProcess.endingMessage(roundNumber));  
+      // This compared the win count of the computer and user, and outputs a message depending on if the user won or loss or it is a tie
+      gameProcess.printWinOrLoss();            
+   }
 }
